22d31078fc2723009a32e8ad3f61f400893b7581
Jose <jozylopez27@gmail.com>
2020-02-27 15:56:07 -0600

    test refactoring all tests work

--- test/bookmark-endpoints.spec.js
+++ test/bookmark-endpoints.spec.js
@@ -14,35 +14,57 @@ describe.only('Bookmarks Endpoints', function() {
         app.set('knexInstance', knexInstance)
     })
 
-    after('disconnect from knex Instance after testing', () => {
-        knexInstance.destroy() })
+    after('disconnect from knex Instance after testing', () => knexInstance.destroy())
 
-    before('clean the bookmark table before testing', () => { 
-        knexInstance('bookmarks_table').truncate() })
+    before('clean the bookmark table before testing', () => knexInstance('bookmarks_table').truncate())
 
-    afterEach('clean the bookmarks table of data', () => { 
-        knexInstance('bookmarks_table').truncate() })
+    afterEach('clean the bookmarks table of data', () => knexInstance('bookmarks_table').truncate())
     
-    context(`Given there are bookmarks in the database.`, () => {
-        const testBookmarks = makeBookmarksArray()
+    describe(`GET /articles`, () => {
 
-        beforeEach('insert bookmarks', () => {
-             knexInstance('bookmarks_table')
-                .insert(testBookmarks)
+        context(`Given no bookmarks`, () => {
+            it(`responds with 200 and an empty list`, () => {
+                return supertest(app)
+                    .get('/bookmarks')
+                    .expect(200, [])
+            })
         })
 
-        it(`GET /bookmarks responds with 200 and all bookmarks are returned`, () => {
-            return supertest(app)
-                .get('/bookmarks')
-                .expect(200, testBookmarks)
-        })
+        context(`Given there are bookmarks in the database.`, () => {
+            const testBookmarks = makeBookmarksArray()
 
-        it(`Get /bookmarks/:id responds with 200 and the specified bookmark`, () => {
-            const bookmarkId = 2
-            const expectedBookmark = testBookmarks[bookmarkId - 1]
-            return supertest(app)
-                .get(`/bookmarks/${bookmarkId}`)
-                .expect(200, expectedBookmark)
+            beforeEach('insert bookmarks', () => {
+                return knexInstance
+                    .into('bookmarks_table')
+                    .insert(testBookmarks)
+            })
+
+            it(`GET /bookmarks responds with 200 and all bookmarks are returned`, () => {
+                return supertest(app)
+                    .get('/bookmarks')
+                    .expect(200, testBookmarks)
+            })
+        })
+    })
+
+
+    describe(`GET /bookmarks/:id`, () => {
+        context(`Given there are articles in the database`, () => {
+            const testBookmarks = makeBookmarksArray()
+
+            beforeEach('insert bookmarks', () => {
+                return knexInstance
+                    .into('bookmarks_table')
+                    .insert(testBookmarks)
+            })
+
+            it(`Get /bookmarks/:id responds with 200 and the specified bookmark`, () => {
+                const bookmarkId = 2
+                const expectedBookmark = testBookmarks[bookmarkId - 1]
+                return supertest(app)
+                    .get(`/bookmarks/${bookmarkId}`)
+                    .expect(200, expectedBookmark)
+            })
         })
     })
 })
